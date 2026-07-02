185cb2ecedeff8cf8f5e604e16441357e694a681
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2026-07-02 15:50:20 +0700

    feat: add home view

---
+++ web/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="row w-100 justify-content-center">
+      <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 shadow py-2 rounded">
+        <div class="row">
+          <div class="col-12">
+            <input type="text" class="form-control" placeholder="Name" aria-label="Name" />
+          </div>
+        </div>
+        <div class="row mt-2 gy-2">
+          <div class="col-12 text-center">
+            <button class="btn btn-primary">Create room</button>
+          </div>
+          <div class="col-12 text-center">
+            <button class="btn btn-secondary">Join room</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
